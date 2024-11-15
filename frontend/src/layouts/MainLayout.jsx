9566421b5fb2c4ce38bf9ae125c5d44454963296
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-11-15 16:53:19 +0530

    fixed the style

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -50,7 +50,7 @@ const MainLayout = ({ toggleTheme, darkMode }) => {
       />
       <div className=" bg-gray-50 min-h-screen dark:bg-gray-900 flex md:flex-row flex-col ">
         <Sidebar />
-        <div className="p-7  flex-1 h-screen text-2xl font-semibold">
+        <div className="p-7  flex-1 h-screen text-2xl font-semibold overflow-y-auto">
           <div className=" flex justify-end">
             <button
               className="text-dark-purple hidden md:block mb-5 px-5 py-2 rounded dark:text-white"
